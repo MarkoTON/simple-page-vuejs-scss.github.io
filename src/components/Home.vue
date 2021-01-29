8c0c3d2409e218ade9c29c89c596fd698f2cac09
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-01-29 12:22:18 +0100

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+h1, h2 {
+  font-weight: normal;
+  color:red;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
